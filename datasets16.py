d66dbc2bc6361e1720896f00036d463228616174
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2026-01-31 02:59:37 +0100

    add Weibo2014 dataset
    Temporarily disable CNN, as there is issue with n_jobs=-1
    Fix duplicated counts in results when 64 and 16-channel datasets are evaluated concurently

--- datasets16.py
+++ datasets16.py
@@ -1,7 +1,4 @@
-"""Physionet Motor Imagery dataset with 16 channels."""
-
-from moabb.datasets import PhysionetMI
-
+from moabb.datasets import PhysionetMI, Weibo2014
 
 class PhysionetMI16(PhysionetMI):
     """
@@ -14,6 +11,30 @@ class PhysionetMI16(PhysionetMI):
     def __init__(self, imagined=True, executed=False):
         super().__init__(imagined=imagined, executed=executed)
         self.code = "PhysionetMotorImagery16"
+        self.selected_channels = [
+            'FCz', 'Pz', 'FC2', 'Cz', 'FC4', 'C3', 'CP1', 'C2', 
+            'CP4', 'CP3', 'CP2', 'C4', 'CPz', 'FC3', 'C1', 'FC1'
+        ]
+
+    def _load_one_run(self, subject, run, preload=True):
+        # Load the original data (64 ch)
+        raw = super()._load_one_run(subject, run, preload=preload)
+        # Pick only the selected 16 channels
+        raw.pick_channels(self.selected_channels)
+        
+        return raw
+
+class Weibo2014_16(Weibo2014):
+    """
+    Weibo2014 dataset with 16 selected channels.
+    
+    The channels are motor-cortex focused and include:
+    ['FCz', 'Pz', 'FC2', 'Cz', 'FC4', 'C3', 'CP1', 'C2', 'CP4', 'CP3', 'CP2', 'C4', 'CPz', 'FC3', 'C1', 'FC1']
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.code = "Weibo2014_16"
         self.selected_channels = [
             'FCz', 'Pz', 'FC2', 'Cz', 'FC4', 'C3', 'CP1', 'C2', 
             'CP4', 'CP3', 'CP2', 'C4', 'CPz', 'FC3', 'C1', 'FC1'
